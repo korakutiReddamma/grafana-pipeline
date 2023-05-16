1b3d819991684a8b9629afd7d3c13150d9ee8127
korakutiReddamma <korakutireddy@gmail.com>
2023-05-16 15:17:24 +0530

    update

--- dependeic.sh
+++ dependeic.sh
@@ -4,6 +4,6 @@ yum update -y
 cd /opt
 wget https://dl.grafana.com/enterprise/release/grafana-enterprise-9.5.2.linux-amd64.tar.gz
 tar -zxvf grafana-enterprise-9.5.2.linux-amd64.tar.gz
-chmod -R 755 grafana-9.2.1
-cd grafana-9.2.1/bin/
+chmod -R 755 grafana-9.5.2
+cd grafana-9.5.2/bin/
 nohup ./grafana-server &
