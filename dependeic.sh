0f199d4043a74e93c0077d86f13c85203d580f3a
korakutiReddamma <korakutireddy@gmail.com>
2023-05-16 14:24:28 +0530

    modify files

--- dependeic.sh
+++ dependeic.sh
@@ -2,8 +2,8 @@
 sudo su -
 yum update -y
 cd /opt
-wget https://dl.grafana.com/oss/release/grafana-9.2.1.linux-amd64.tar.gz
-tar -xvzf grafana-9.2.1.linux-amd64.tar.gz
+wget https://dl.grafana.com/enterprise/release/grafana-enterprise-9.5.2.linux-amd64.tar.gz
+tar -zxvf grafana-enterprise-9.5.2.linux-amd64.tar.gz
 chmod -R 755 grafana-9.2.1
 cd grafana-9.2.1/bin/
 nohup ./grafana-server &
